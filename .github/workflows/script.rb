7433f3c9a1b838177d0bfde1445037074b9b6ce1
specter78 <40606342+specter78@users.noreply.github.com>
2024-05-23 10:45:06 +0530

    refactor

---
+++ .github/workflows/script.rb
@@ -0,0 +1,17 @@
+
+array = ['MANDEEP']
+
+if array.length > 0
+  system('npm i -g stock-nse-india')
+  system('yarn global add carbon-now-cli')
+end
+
+array.each do |stock|
+  output = `nseindia equity #{stock}`
+  price = output.split("\n")[19].split("|")[2].strip
+  caption = stock + " - " + price
+  system("nseindia equity #{stock} | carbon-now --save-as=output")
+  url = `curl -F'file=@output.png' https://0x0.st`
+  system("curl 'https://api.telegram.org/bot$BOT/sendPhoto?chat_id=-$CHAT&photo=$URL&caption=#{caption}'")
+end
+
