d02561b65b6449d33a359dbfffa611dedf7228d0
specter78 <40606342+specter78@users.noreply.github.com>
2024-05-24 10:05:49 +0530

    update

--- script.rb
+++ script.rb
@@ -13,7 +13,7 @@ end
 stocks.each do |stock|
   system("nseindia equity #{stock} | carbon-now --save-as=output")
   output = `nseindia equity #{stock}`
-  caption = "#{stock} - ₹#{output.split("\n")[19].split(' ')[-2]}\n#{output.split("\n")[-2]}"
+  caption = "#{stock} - ₹#{output.split("\n")[19].split(' ')[-2]}\n#{output.split("\n")[-2]}\n#{output.split("\n")[3].split(''')[1]}"
   path_to_photo = File.expand_path('./output.png')
   bot.api.send_photo(chat_id: chat_id, photo: Faraday::UploadIO.new(path_to_photo, 'image/png'), caption: caption)
 end
