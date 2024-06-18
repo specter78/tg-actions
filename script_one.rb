f3c8e318332ca778f28aad428334560068f23610
specter78 <40606342+specter78@users.noreply.github.com>
2024-06-18 12:49:58 +0530

    Update script_one.rb

--- script_one.rb
+++ script_one.rb
@@ -4,7 +4,7 @@ token = ENV["BOT"]
 chat_id = ENV["CHAT"]
 bot = Telegram::Bot::Client.new(token)
 
-stocks = [['MANDEEP', 60]]
+stocks = [['MANDEEP', 65]]
 stocks.each do |stock, price|
   output = File.read(stock + '.txt').split("\n")[0..-14]
   exit if output[3].split("'")[1].split(" ")[1] == "16:00:00"
