1b82553d78fca04e9d103a60148120d5a82ac1bd
specter78 <40606342+specter78@users.noreply.github.com>
2024-10-21 16:24:58 +0530

    update

--- script_one.rb
+++ script_one.rb
@@ -5,7 +5,7 @@ chat_id = ENV["CHAT"]
 bot = Telegram::Bot::Client.new(token)
 
 # stock > price
-stocks = [['MANDEEP', 65]]
+stocks = [['WCIL', 165]]
 stocks.each do |stock, price|
   output = File.read(stock + '.txt').split("\n")[0..-14]
   exit if output[3].split("'")[1].split(" ")[1] == "16:00:00"
@@ -16,12 +16,12 @@ stocks.each do |stock, price|
 end
 
 # stock < price
-stocks = [['MAXPOSURE', 80], ['APS', 350]]
-stocks.each do |stock, price|
-  output = File.read(stock + '.txt').split("\n")[0..-14]
-  exit if output[3].split("'")[1].split(" ")[1] == "16:00:00"
-  exit if output[19].split(' ')[-2].to_i > price
-  caption = "#{stock} - \u20B9#{output[19].split(' ')[-2]}\n#{output[-2]}\n#{output[3].split("'")[1]}"
-  path_to_photo = File.expand_path("./#{stock}.png")
-  bot.api.send_photo(chat_id: chat_id, photo: Faraday::UploadIO.new(path_to_photo, 'image/png'), caption: caption)
-end
+# stocks = [['MAXPOSURE', 80], ['APS', 350]]
+# stocks.each do |stock, price|
+#   output = File.read(stock + '.txt').split("\n")[0..-14]
+#   exit if output[3].split("'")[1].split(" ")[1] == "16:00:00"
+#   exit if output[19].split(' ')[-2].to_i > price
+#   caption = "#{stock} - \u20B9#{output[19].split(' ')[-2]}\n#{output[-2]}\n#{output[3].split("'")[1]}"
+#   path_to_photo = File.expand_path("./#{stock}.png")
+#   bot.api.send_photo(chat_id: chat_id, photo: Faraday::UploadIO.new(path_to_photo, 'image/png'), caption: caption)
+# end
